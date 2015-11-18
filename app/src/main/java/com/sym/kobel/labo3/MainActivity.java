bab32954f95c23f5239ff973196be29ffe35ca7a
haurane <haurane@Kobel-TP-Deb.home>
2015-11-18 10:42:09 +0100

    Layout and start of authentification levels

--- app/src/main/java/com/sym/kobel/labo3/MainActivity.java
+++ app/src/main/java/com/sym/kobel/labo3/MainActivity.java
@@ -5,6 +5,8 @@ import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,19 +14,48 @@ public class MainActivity extends Activity {
 
     private static final String APP_TAG = "labo3";
 
+    /* View related elements */
+    private EditText editMail;
+    private EditText editPWD;
     private TextView textViewResult;
+    private Button loginButton;
+
+    /* Stuff for NFC */
     private NfcAdapter nfcAdapter;
+
+    /* Variables for checks */
+    private String login, passwd;
+
+    private boolean checkCredentials(){
+        login = editMail.getText().toString();
+		passwd = editPWD.getText().toString();
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewResult = (TextView) findViewById(R.id.textViewResult);
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        editMail = (EditText) findViewById(R.id.editMail);
+        editPWD = (EditText) findViewById(R.id.editPWD);
+        loginButton = (Button) findViewById(R.id.loginButton);
 
-        if(nfcAdapter == null){
+        //nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        /* Check if Nfc is available */
+        /*if(nfcAdapter == null){
             Toast.makeText(this,"No NFC available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
         }
+
+        if(!nfcAdapter.isEnabled()){
+            textViewResult.setText("Ooops nfc is disabled");
+        } else {
+            textViewResult.setText("We have nfc");
+        }*/
+
 
 
     }
